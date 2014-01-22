49d8455c5739942949c9402e36ea3d0a20210fa9
deweyvm <deweyvm@gmail.com>
2014-01-22 14:30:55 -0500

    changed parameters

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-procname=python
-user=doge
+procname="python"
+chuid="doge:doge"
+exe="/bin/bash"
+args=-c "python /home/doge/whatever/build-server.py" > /var/log/doge/error.log 2>&1
 num=`pgrep $procname | wc -l`
 if [[ $num -gt 1 ]] ; then
     echo "Found more than one process to kill:"
@@ -9,6 +11,6 @@ if [[ $num -gt 1 ]] ; then
     exit 1
 fi
 killall $procname &> /dev/null
-/sbin/start-stop-daemon --background --start -o --user $user --name build-server --no-close --chuid doge:doge --exec /bin/bash -- -c "python /home/doge/whatever/build-server.py" > /var/log/doge/error.log 2>&1
+/sbin/start-stop-daemon --background --start --no-close --chuid $chuid --exec /bin/bash -- $args
 
 #/sbin/start-stop-daemon -b -S --user doge --name dogue-server --chuid doge --exec /usr/bin/java -- -jar /home/doge/whatever/whatever_server_jar/whatever-server.jar
