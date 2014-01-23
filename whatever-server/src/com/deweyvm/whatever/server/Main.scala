e176fa632cfe05251a8fc00c05843eff70393c10
deweyvm <deweyvm@gmail.com>
2014-01-22 20:18:47 -0500

    fixed server and logging

--- whatever-server/src/com/deweyvm/whatever/server/Main.scala
+++ whatever-server/src/com/deweyvm/whatever/server/Main.scala
@@ -1,10 +1,22 @@
 package com.deweyvm.whatever.server
 
+import com.deweyvm.gleany.logging.Logger
+import com.deweyvm.whatever.common.Implicits._
+
 object Main {
   def main(args:Array[String]) {
     if (args.contains("--pass")) {
       System.exit(0)
     }
+    val logIndex = args.indexOf("--log")
+    if (logIndex != -1) {
+      val logdir = args(logIndex + 1)
+      Server.logdir = logdir.some
+
+
+    }
+    Logger.attachCrasher(false, Server.logdir.getOrElse("."))
+
     new Server().start()
   }
 }
--- whatever-server/src/com/deweyvm/whatever/server/Server.scala
+++ whatever-server/src/com/deweyvm/whatever/server/Server.scala
@@ -4,13 +4,37 @@ import java.net.{SocketTimeoutException, ServerSocket}
 import com.deweyvm.gleany.Debug
 import com.deweyvm.gleany.net.Task
 import com.deweyvm.whatever.common.Implicits._
+import java.util.{Date, Calendar}
+import java.text.SimpleDateFormat
+import java.io.{FileOutputStream, File}
+import com.deweyvm.whatever.common.data.Encoding
 
+object Server {
+  var logdir:Option[String] = None
+  val dateStr = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date)//fixme -- code clone with logger
+  val filename = "log_%s" format dateStr
+  val logfile = logdir map { f => f + "/" +  filename }  getOrElse ("./" + filename)
+
+  val file = {
+    val file = new File(logfile)
+    if (!file.exists()) {
+      file.createNewFile()
+    }
+    new FileOutputStream(file, false)
+  }
+  def log(string: String, stackOffset: Int = 2) {
+    val callStack = Thread.currentThread().getStackTrace
+    val className = callStack(stackOffset).getClassName.split( """[.]""").last.replace("$", "")
+    val s = "%s: %s".format(className, string)
+    file.write(Encoding.toBytes(s))
+  }
+}
 
 class Server extends Task {
   val port = 4815
   var running = true
   var currentReader:Option[Reader] = None
-
+  Server.log("hello")
   override def execute() {
     Debug.debug("Starting server")
     val server = new ServerSocket(port)
