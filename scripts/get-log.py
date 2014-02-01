a63b7bc6f936259967e60459ded75afacd2153b1
deweyvm <deweyvm@gmail.com>
2014-01-31 19:59:24 -0500

    echo command for easier repeating

--- scripts/get-log.py
+++ scripts/get-log.py
@@ -38,6 +38,7 @@ def main():
     else:
         prefix, date = max(all_matching, key=lambda p: p[1])
         path = os.path.join(loc, prefix + "_" + str(date))
+        print("tail -f %s" % path)
         os.execl("/usr/bin/tail", "/usr/bin/tail", "-f", path)
 
 def get_date(s):
